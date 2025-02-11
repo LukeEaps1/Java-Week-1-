08ad4beff214a58e95a8e7c7ad0dc840763302bd
LukeEaps1 <lukeeaps06@gmail.com>
2025-02-11 10:10:44 -0500

    wefwf

---
+++ Comments.java
@@ -0,0 +1,13 @@
+/************************************
+*Project: Demo for Comments
+*Programmer: Luke Eapen
+*Date: February 11 2025
+*Program Name: Comments.java
+*************************************/
+public class Comments {
+    public static void main (String[ ] args)  
+    {
+    System.out.println("Hi"); //message to user
+    System.out.println("LabOneA"); //assignment
+    }
+}
